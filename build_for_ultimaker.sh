8c0962a0951f5844c12e4353e0919b912f396986
Jaime van Kessel <nallath@gmail.com>
2016-06-23 19:41:00 +0200

    Added build for ultimaker script
    This ensures that the UM package script understands it

---
+++ build_for_ultimaker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Treat unset variables as an error when substituting.
+set -u
+
+# Re-create build and move there. 
+rm -rf _build_armhf
+mkdir _build_armhf
+cd _build_armhf
+
+cmake ../cmake -DCMAKE_TOOLCHAIN_FILE=$1 -Dprotobuf_BUILD_TESTS=OFF -Dprotobuf_BUILD_EXAMPLES=OFF
+make
